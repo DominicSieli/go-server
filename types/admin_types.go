8b1e59e5743ee9f5fb69b7ba1a5a2e7b4db965ea
DominicSieli <dominicsieli@gmail.com>
2025-12-07 00:48:17 -0700

    updated types

--- types/admin_types.go
+++ types/admin_types.go
@@ -2,29 +2,23 @@ package types
 
 import "time"
 
-type AdminServices interface {
-	CreateAdmin(Admin) error
-	GetAdminByID(id int) (*Admin, error)
-	GetAdminByEmail(email string) (*Admin, error)
-}
-
 type Admin struct {
-	ID        int       `json:"id"`
-	FirstName string    `json:"firstName"`
-	LastName  string    `json:"lastName"`
-	Email     string    `json:"email"`
-	Password  string    `json:"-"`
+	ID		  int		`json:"id"`
+	FirstName string	`json:"firstName"`
+	LastName  string	`json:"lastName"`
+	Email	  string	`json:"email"`
+	Password  string	`json:"-"`
 	CreatedAt time.Time `json:"createdAt"`
 }
 
-type AdminRegisterPayload struct {
+type CreateAdminPayload struct {
 	FirstName string `json:"firstName" validate:"required"`
 	LastName  string `json:"lastName" validate:"required"`
-	Email     string `json:"email" validate:"required,email"`
+	Email	  string `json:"email" validate:"required,email"`
 	Password  string `json:"password" validate:"required,min=8,max=128"`
 }
 
-type AdminLoginPayload struct {
-	Email    string `json:"email" validate:"required,email"`
+type LoginAdminPayload struct {
+	Email	 string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required"`
 }
--- types/user_types.go
+++ types/user_types.go
@@ -2,29 +2,23 @@ package types
 
 import "time"
 
-type UserServices interface {
-	CreateUser(User) error
-	GetUserByID(id int) (*User, error)
-	GetUserByEmail(email string) (*User, error)
-}
-
 type User struct {
-	ID        int       `json:"id"`
-	FirstName string    `json:"firstName"`
-	LastName  string    `json:"lastName"`
-	Email     string    `json:"email"`
-	Password  string    `json:"-"`
+	ID		  int		`json:"id"`
+	FirstName string	`json:"firstName"`
+	LastName  string	`json:"lastName"`
+	Email	  string	`json:"email"`
+	Password  string	`json:"-"`
 	CreatedAt time.Time `json:"createdAt"`
 }
 
-type UserRegisterPayload struct {
+type CreateUserPayload struct {
 	FirstName string `json:"firstName" validate:"required"`
 	LastName  string `json:"lastName" validate:"required"`
-	Email     string `json:"email" validate:"required,email"`
+	Email	  string `json:"email" validate:"required,email"`
 	Password  string `json:"password" validate:"required,min=8,max=128"`
 }
 
-type UserLoginPayload struct {
-	Email    string `json:"email" validate:"required,email"`
+type LoginUserPayload struct {
+	Email	 string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required"`
 }
